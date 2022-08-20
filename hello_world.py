3857d9f31677166bb3bed347f464ef13db28fe05
Mikko <mikko.tuumanen@tuni.fi>
2022-08-20 17:13:04 +0300

    helloworld fixed

--- hello_world.py
+++ hello_world.py
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-print('Hello World!')
-=======
 print("Hello world!")
-<<<<<<< HEAD
->>>>>>> b23547b242012497282910300f6e6c1d5a27df82
-=======
 print("Hello once again")
->>>>>>> intermediate/feature/add-logging
-=======
-print('Hello world!')
->>>>>>> 3e266f78da66933e5fddfbbc8b78bb50a693d527
+
